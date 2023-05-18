4ca3afc20131c874528386c5fda2ef58372f1096
Marceau Malégeant <soetchdev@gmail.com>
2023-05-18 12:27:04 +0200

    Add topic support through the parser Fixes #1

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+title1 = getJson(1, "title")
+jptitle1 = getJson(1, "jptitle")
+hiratitle1 = getJson(1, "hiratitle")
+romtitle1 = getJson(1, "romtitle")
+
+title1.then(function(title) {
+    title1 = title
+    console.log(title1)
+})
